622ca5e1215e92e92afa6e28195c8463288f5a3d
stankovicja <stankovic1107@gmail.com>
2015-09-03 19:08:36 +0200

    first  commit

---
+++ IepProject/IepProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IepProject.MvcApplication" Language="C#" %>
---
+++ IepProject/IepProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IepProject.Startup))]
+namespace IepProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
